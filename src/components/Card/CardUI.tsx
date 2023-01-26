4faacc88eb870a30e87ea8f6747a9c4cd84c07b2
Samokhin Daniil <daniil.samokhin@decathlon.com>
2023-01-26 16:01:01 +0300

    Card Image

--- src/components/Card/CardUI.tsx
+++ src/components/Card/CardUI.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+
 type Props = {};
 
 const CardUI = (props: Props) => {
-	return <div className='container'>CardUI</div>;
+	return (
+		<div className='container'>
+			<div className='card' style={{ width: '18rem' }}>
+				<Image src='...' className='card-img-top' alt='...' />
+				<div className='card-body'>
+					<h5 className='card-title'>Название карточки</h5>
+					<p className='card-text'>
+						Some quick example text to build on the card title and make up the bulk of the card's
+						content.
+					</p>
+					<a href='#' className='btn btn-primary'>
+						Переход куда-нибудь
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CardUI;
